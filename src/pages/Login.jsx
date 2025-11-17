efe884b99c45bf2ba2c47fe1c02458e83b15c1a2
Mohammad Mirji <shehbazmirji@gmail.com>
2025-11-17 22:30:23 +0530

    Update login API URL to use environment variable

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:3000/api/auth/login", formData);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, formData);
       if (res.data.success) {
         login(res.data.token, res.data.user);
         alert("Login successful!");
